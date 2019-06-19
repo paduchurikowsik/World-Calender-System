eef6b4996af41bc9aa96858bfce3f836d72b53b6
kowsik paduchuri <p.kowsik5@gmail.com>
2019-06-19 14:01:06 -0400

    Part 1

---
+++ Assets/Scripts/WorldCalender.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class WorldCalender : MonoBehaviour
+{
+    DateTime StartDateTime;
+    DateTime CurrentDateTime;
+
+    float StartTime;
+
+    void Start()
+    {
+        StartDateTime = new DateTime(1, 1, 1, 0, 0, 0);
+        StartTime = Time.time;
+    }
+
+
+    void FixedUpdate()
+    {
+        CurrentDateTime = StartDateTime + System.TimeSpan.FromSeconds((Time.time - StartTime) * 60);
+        DebugCalender();
+    }
+
+    void DebugCalender(){
+        Debug.Log ("Year:" + CurrentDateTime.Year+", Month:" + CurrentDateTime.Month+", Day:" + CurrentDateTime.Day+", Hour:" + CurrentDateTime.Hour+", Minute:"+CurrentDateTime.Minute);
+    }
+}
